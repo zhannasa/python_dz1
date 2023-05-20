d11519b90bd58a49d7812e95297ef327cc8f68b0
zhannas <zhannas@gmail.com>
2023-05-20 21:21:04 +0200

    task6

---
+++ task6.py
@@ -0,0 +1,11 @@
+first_digit = int(input('Enter the first digit: ')) 
+second_digit = int(input('Enter the second digit: '))   
+third_digit = int(input('Enter the third digit: '))   
+fourth_digit = int(input('Enter the fourth digit: ')) 
+fifth_digit = int(input('Enter the fifth digit: ')) 
+sixth_digit = int(input('Enter the sixth digit: ')) 
+
+if (first_digit + second_digit + third_digit == fourth_digit + fifth_digit + sixth_digit):
+    print('Yes')
+else:
+    print('No')
